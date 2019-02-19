0818ebee05972139c65d1f0a26950e53fb0a673d
Justin Needham <justin.needham@alumnimail.pepperdine.edu>
2019-02-19 07:10:40 -0700

    init files

---
+++ InteractiveStory/Page.swift
@@ -0,0 +1,10 @@
+//
+// Created by Justin Needham on 2019-02-19.
+// Copyright (c) 2019 ___jneedham___. All rights reserved.
+//
+
+import Foundation
+
+struct Page {
+    let story: Story
+}
---
+++ InteractiveStory/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  InteractiveStory
+//
+//  Created by Justin Needham on 2019-02-18.
+//  Copyright © 2019 ___jneedham___. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}

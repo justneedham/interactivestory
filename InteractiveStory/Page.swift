996370ab32e1d8c0ec9764180620ea15c308c453
Justin Needham <justin.needham@alumnimail.pepperdine.edu>
2019-02-19 07:44:02 -0700

    addchoicewith

--- InteractiveStory/Page.swift
+++ InteractiveStory/Page.swift
@@ -5,6 +5,36 @@
 
 import Foundation
 
-struct Page {
+class Page {
     let story: Story
+    typealias Choice = (title: String, page: Page)
+    var firstChoice: Choice?
+    var secondChoice: Choice?
+
+    init(story: Story) {
+        self.story = story
+    }
+}
+
+extension Page {
+
+    func addChoiceWith(title: String, story: Story) -> Page {
+        let page = Page(story: story)
+        return addChoiceWith(title: title, page: page)
+    }
+
+    func addChoiceWith(title: String, page: Page) -> Page {
+//        guard firstChoice != nil && secondChoice != nil else { return self }
+//        if firstChoice == nil {
+//            firstChoice = (title, page)
+//        } else {
+//            secondChoice = (title, page)
+//        }
+        switch (firstChoice, secondChoice) {
+        case (.some, .some): return self
+        case(.none, .none), (.none, .some): firstChoice = (title, page)
+        case(.some, .none): secondChoice = (title,page)
+        }
+        return page
+    }
 }

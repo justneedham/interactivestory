b9431621aa5702e52b4a56059991806ad80e3e24
Justin Needham <justin.needham@alumnimail.pepperdine.edu>
2019-02-20 17:40:51 -0700

    add image for first page

--- InteractiveStory/PageController.swift
+++ InteractiveStory/PageController.swift
@@ -12,6 +12,12 @@ class PageController: UIViewController {
 
     var page: Page?
 
+    // MARK: - User Interface Properties
+    let artworkView = UIImageView()
+    let storyLabel = UILabel()
+    let firstChoiceButton = UIButton(type: .system)
+    let secondChoiceButton = UIButton(type: .system)
+
     required init?(coder adDecoder: NSCoder) {
         super.init(coder: adDecoder)
     }
@@ -25,9 +31,22 @@ class PageController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        view.backgroundColor = .blue
         if let page = page {
-            print(page.story.text)
+            artworkView.image = page.story.artwork
         }
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+
+        view.addSubview(artworkView)
+        artworkView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            artworkView.topAnchor.constraint(equalTo: view.topAnchor),
+            artworkView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            artworkView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            artworkView.leftAnchor.constraint(equalTo: view.leftAnchor),
+        ])
     }
 }
